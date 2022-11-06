e35ff9dac738f9bdde1a7da113f893d2e5d435f2
ajr3021 <54864764+ajr3021@users.noreply.github.com>
2022-11-06 10:39:09 -0500

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,5 +1,5 @@
 ROCHESTER_LATITUDE = 43.1566
-ROCHESTER_LONGITUDE = 77.6088
+ROCHESTER_LONGITUDE = -77.6088
 
 """
 Given a csv file containing the following header:
@@ -14,8 +14,10 @@ def get_data(filename):
 Create a custom sorting function to sort by nearest value to rochester,
 based on the sum of the difference between the values latitude and rochester's
 latitude and the difference between the values longitude and rochester's longitude.
+
+Note: Difference should be in absolute value
 """
-def sort_by_latitude(value):
+def sort_by_latitude_longitude(value):
     pass
 
 
